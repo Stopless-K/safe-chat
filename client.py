7c7a0a7eee0b53ea80581a6329c03f527467bd37
indestinee Chen <indestinee@gmail.com>
2017-12-08 00:36:16 +0800

    updated

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import socket
+import socket, threading
 from IPython import embed
 
 encoding = 'utf-8'
@@ -15,25 +15,52 @@ def byte_to_str(x):
 
 class Client(object):
     def __init__(self, ip='127.0.0.1', port=23333, history_size=100, encode=str_to_byte, decode=byte_to_str):
-        print('[OPR] connecting to {}:{} ..'.format(ip, port))
+        print('[RUN] connecting to {}:{} ..'.format(ip, port))
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((ip, port))
+        self.decode = decode
+        self.encode = encode
         self.result = Result()
-        if self.socket.recv(1024) == self.result.connect_success:
+        res = self.socket.recv(1024)
+        if res == self.result.connect_success:
             print('[SUC] {}:{} connected ..'.format(ip, port))
-            self.status = 'success'
+            
+            data = self.socket.recv(1024)
+            assert self.decode(data) == '--! username'
+            username = input('username: ')
+            self.socket.send(self.encode(username))
+            
+            data = self.socket.recv(1024)
+            assert self.decode(data) == '--! password'
+            password = input('password: ')
+            self.socket.send(self.encode(password))
+
+            res = self.decode(self.socket.recv(1024))
+            print(res)
+            if res[1:4] == 'END':
+                self.status = 'fail'
+            else:
+                self.status = 'success'
         else:
             print('[ERR] connecting to {}:{} failed ..'.format(ip, port))
             self.status = 'fail'
         self.history = [''] * history_size
         self.history_size = history_size
         self.history_pointer = 0
-        self.decode = decode
-        self.encode = encode
         self.cnt = 0
+
+    def auto_receive(self):
+        while True:
+            data = self.decode(self.socket.recv(1024))
+            if not data:
+                break
+            print(data)
+
 
     def send(self, s, resend=False):
         self.socket.send(self.encode(s))
+
+        return True
         if not resend:
             self.history[self.history_pointer] = 1
             self.history_pointer = (self.history_pointer + 1) % self.history_size
@@ -59,22 +86,29 @@ class Client(object):
         ], resend=True)
 
     def free_style(self):
+        t = threading.Thread(target=self.auto_receive, args=())
+        t.start()
         while True:
+            # data = input3('order: ')
             data = input()
             self.send(data)
+        t.join()
 
 
     def __del__(self):
         pass
 
 if __name__ == '__main__':
-    try:
-        with open('./server.txt', 'r') as f:
-            ip, port = f.read().split(' ')
-        port = int(port)
-        client = Client(ip, port)
-    except:
-        client = Client()
+    # try:
+    #     with open('./server.txt', 'r') as f:
+    #         ip, port = f.read().split(' ')
+    #     port = int(port)
+    #     client = Client(ip, port)
+    # except:
+    #     client = Client()
+    client = Client()
+    if client.status == 'fail':
+        client = None
     client.free_style()
 
     
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, threading, time
+import socket, threading, time, queue
 encoding = 'utf-8'
 
 class Result:
@@ -11,30 +11,109 @@ def str_to_byte(x):
 def byte_to_str(x):
     return x.decode(encoding)
 
+def no_checking(username, password):
+    if password == '123456':
+        return username
+    return None
+
 class Server(object):
-    def __init__(self, port=23333, encode=str_to_byte, decode=byte_to_str):
+    def __init__(self, port=23333, encode=str_to_byte, decode=byte_to_str, check_user=no_checking):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(('0.0.0.0', port))
+        print('[RUN] port: {}'.format(port))
         self.socket.listen(5)
         self.result = Result()
         self.decode = decode
         self.encode = encode
+        self.login_queue = queue.Queue()
+        self.check_user = check_user
+        self.online_dict = {}
+        self.sending_queue = queue.Queue()
+
+    def login(self, sock):
+        sock.send(self.encode('--! username'))
+        username = self.decode(sock.recv(1024))
+        sock.send(self.encode('--! password'))
+        password = self.decode(sock.recv(1024))
+        return self.check_user(username, password)
+
+    def anaylize(self, data, user):
+        if data[:4] == '--! ':
+            print('[RUN] order mode ..')
+            data = data[4:].split(' ')
+            if data[0] == 'to':
+                self.online_dict[user]['chat_to'] = data[1]
+                print('[RUN] {} is talking to {} ..'.format(user, data[1]))
+        else:
+            chat_to = self.online_dict[user]['chat_to']
+            # print('[RUN] {} are sending to {}'.format(user, chat_to))
+            # print('[LOG] online users: {}'.format(self.online_dict))
+            if chat_to in self.online_dict and chat_to != user:
+                self.sending_queue.put([
+                    self.online_dict[chat_to]['sock'],
+                    self.encode(user + ': ' + data),
+                ])
+                return None
+            else:
+                return '[ERR] {} is not online!'.format(chat_to)
+        
+        return None
+
+        
+    def send(self, sock, data):
+        sock.send(data)
+        return True
+        # return sock.recv(1024) == send_success
+
+    def auto_send(self):
+        print('[RUN] auto_send started ..')
+        while True:
+            sock, data = self.sending_queue.get()
+            # print('[RUN] data got! sending ..')
+            while not self.send(sock, data):
+                # print('[ERR] send failed, resending ..')
+                pass
 
     def tcplink(self, sock, addr):
         print('[LIN] address: {}'.format(addr))
         sock.send(self.result.connect_success)
+        user = self.login(sock)
+        if not user:
+            res = '[END] login failed ..'
+            sock.send(self.encode(res))
+            return
+        sock.send(self.encode('[SUC] login successfully!'))
+
+        self.online_dict[user] = {'sock': sock, 'addr': addr, 'chat_to': '----', 'username': user}
+
         while True:
-            data = sock.recv(1024)
-            sock.send(self.result.send_success)
+            data = self.decode(sock.recv(1024))
             if not data:
                 break
-            print('[GET] data: {}'.format(self.decode(data)))
+            # print('[GET] data: {}'.format(data))
+            res = self.anaylize(data, user)
+            # print('[RUN] add to sending_queue ..')
+            if res:
+                sock.send(self.encode(res))
+            # print('[SUC] added ..')
         sock.close()
         print('[END] connection from {} closed ..'.format(addr))
         
     def run(self):
-        sock, addr = self.socket.accept()
-        t = threading.Thread(target=self.tcplink(sock, addr))
+        q = []
+        t = threading.Thread(target=self.auto_send, args=())
+        t.start()
+        q.append(t)
+        while True:
+            print('[RUN] server is running now, waiting for new client ..')
+            sock, addr = self.socket.accept()
+
+            t = threading.Thread(target=self.tcplink, args=(sock, addr,))
+            t.start()
+            q.append(t)
+
+        for t in q:
+            t.join()
 
 
 if __name__ == '__main__':
